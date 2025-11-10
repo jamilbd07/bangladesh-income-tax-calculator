6e3b7585261ed93c527568c571e566f8acd546f9
MD Jamil Uddin <jamilbd07@gmail.com>
2025-11-10 22:23:43 +0600

    added prefix in variable

--- block/render.php
+++ block/render.php
@@ -10,21 +10,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Get block attributes
-$theme = isset( $attributes[ 'theme' ] ) ? sanitize_text_field( $attributes[ 'theme' ] ) : 'default';
-$title = isset( $attributes[ 'title' ] ) ? sanitize_text_field( $attributes[ 'title' ] ) : '';
+$bdinctax_selected_theme = isset( $attributes[ 'theme' ] ) ? sanitize_text_field( $attributes[ 'theme' ] ) : 'default';
+$title                   = isset( $attributes[ 'title' ] ) ? sanitize_text_field( $attributes[ 'title' ] ) : '';
 
 // Validate theme
-$allowed_themes = [ 'default', 'dark', 'light' ];
-if ( ! in_array( $theme, $allowed_themes ) ) {
-    $theme = 'default';
+$bdinctax_allowed_themes = [ 'default', 'dark', 'light' ];
+if ( ! in_array( $bdinctax_selected_theme, $bdinctax_allowed_themes ) ) {
+    $bdinctax_selected_theme = 'default';
 }
 
 // Build shortcode attributes
-$shortcode_atts = 'theme="' . esc_attr( $theme ) . '"';
+$bdinctax_shortcode_atts = 'theme="' . esc_attr( $bdinctax_selected_theme ) . '"';
 // Only add title attribute if it's not empty (including whitespace-only strings)
 if ( ! empty( trim( $title ) ) ) {
-    $shortcode_atts .= ' title="' . esc_attr( $title ) . '"';
+    $bdinctax_shortcode_atts .= ' title="' . esc_attr( $title ) . '"';
 }
 
 // Render the shortcode
-echo do_shortcode( '[bd_tax_calculator ' . $shortcode_atts . ']' );
+echo do_shortcode( '[bd_tax_calculator ' . $bdinctax_shortcode_atts . ']' );
